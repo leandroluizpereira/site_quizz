e4b08e2469f3d81389945c9ac8ceb0f765635a8c
leandroluizpereira <leandro.2306@hotmail.com>
2022-09-25 07:08:00 -0300

    Update function.js

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -4,6 +4,11 @@ alert("Hello world");
 
 var element = document.getElementById("btn");
 var text = document.getElementById("text");
+
+var qtdQuestion = [7];
+var question = [];
+
+
 var question = document.getElementById("question");
 
 
@@ -13,4 +18,12 @@ element.onclick = function (){
     alert("onclick , here ");
 
     text.textContent="Correct question !!";
+}
+
+for(i = 0; qtdQuestion<11; i++){
+    
+     question = question +1;
+     console.log(question);
+     qtdQuestion = qtdQuestion+1;
+
 }
