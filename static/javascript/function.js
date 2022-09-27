78e89fb4a760a7cffcbcd1169ec86811d4882800
leandroluizpereira <leandro.2306@hotmail.com>
2022-09-27 01:11:04 -0300

    verify possible create answer

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -5,8 +5,12 @@ alert("Hello world");
 var element = document.getElementById("btn");
 var text = document.getElementById("text");
 var answerOne = document.getElementById("questionOne");
+var answerTwo = document.getElementById("questionTwo");
+var answerThree = document.getElementById("questionThree");
+var answerFour = document.getElementById("questionFour")
 
 let qtdQuestion = [1,2,7];
+//test vetor var question = []
 var question = [];
 let test = 0;
 
@@ -24,17 +28,36 @@ element.onclick = function (){
     for(i=0;i<qtdQuestion.length;i++){
 
         console.log("aqui vai fazer a leitura dos vetores");
-        answerOne.textContent="A primeira pergunta"
-    // criar mais um for aqui com as perguntas colocando o incremento fora do for e fazendo outro for em seguida para proxima pergunta 
+        answerOne.textContent=" the segund answer";
+        answerTwo.textContent="the segund answer";
+        answerThree.textContent="the three answer";
+        answerFour.textContent="the four answer";
+   
+
+    // criar mais um for aqui com as perguntas colocando o incremento fora do for e fazendo outro for em seguida para proxima pergunta
+    
+
+      //aqui se o botão for clicado e for certo  e se o indice do vetor for dois faça e ir pra proxima pergunta 
+
+    // if(&&qtdQuestion == 2){
+
+       // answerTwo.textContent="A segunda pergunta"
+    }
+    
 
     }
+
+    
+
+
+
     questionOne.onclick = function(){
 
         alert("Certa resposta ");
     }
 
 
-}
+
 // denido a variavél , condicional , incremento 
 for(; qtdQuestion.length<10;){
      qtdQuestion.length++;
