16fb1caf3f6704dbf8e2444e682ef809feae8f11
leandroluizpereira <leandro.2306@hotmail.com>
2022-09-27 03:40:06 -0300

    quizz

--- static/javascript/function.js
+++ static/javascript/function.js
@@ -1,38 +1,210 @@
 
-alert("Hello world");
 
 
+//btn questions for selection
+var btnQuestionOne = document.getElementById("btnQuestionOne");
+var btnQuestionTwo = document.getElementById("btnQuestionTwo");
+var btnQuestionThree = document.getElementById("btnQuestionThree");
+var btnQuestionFour = document.getElementById("btnQuestionFour");
+var btnQuestionFive = document.getElementById("btnQuestionFive");
+var btnQuestionSix = document.getElementById("btnQuestionSix");
+var btnQuestionSeven = document.getElementById("btnQuestionSeven");
+var btnQuestionSix = document.getElementById("btnQuestionSix");
+var btnQuestionSeven = document.getElementById("btnQuestionSeven");
+var btnQuestionEight = document.getElementById("btnQuestionEight");
+var btnQuestionNine = document.getElementById("btnQuestionNine");
+var btnQuestionTen = document.getElementById("btnQuestionTen");
+//btn answer e question
 var element = document.getElementById("btn");
+var btnOk = document.getElementById("btnOk");
 var text = document.getElementById("text");
+var question = document.getElementById("question");
 var answerOne = document.getElementById("questionOne");
 var answerTwo = document.getElementById("questionTwo");
 var answerThree = document.getElementById("questionThree");
-var answerFour = document.getElementById("questionFour")
-
+var answerFour = document.getElementById("questionFour");
 let qtdQuestion = [1,2,7];
 //test vetor var question = []
 var question = [];
 let test = 0;
-
-
 var question = document.getElementById("question");
+//Validation question
+var tot = document.getElementById("tot");
+var correct = 0;
 
 
+btnQuestionOne.onclick = function(){       
+    question.textContent="1. question about tecnology , whats is the variable that is constant"
+    answerOne.textContent=" var number = 0;";
+    answerOne.onclick = function(){
+    answerOne.textContent=" is serius , erro ";
+    }
+    answerTwo.textContent=" let number = 0;";
+    answerThree.textContent=" const number = 0;";
+    answerFour.textContent=" int number = 0;";
+}
+btnQuestionTwo.onclick = function (){
+    question.textContent="2. Qual é a impressora que economiza mais tinta "
+    answerOne.textContent=" A Hp torner ";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" B Hp cartucho";
+    answerThree.textContent=" C Hp laser";
+    answerFour.textContent=" D nenhuma das alternativas ";
+}
+btnQuestionThree.onclick = function (){
+    question.textContent="3"
+    answerOne.textContent=" 3";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent="3";
+    answerThree.textContent="3";
+    answerFour.textContent="3";
+}
+btnQuestionFour.onclick = function (){
+    question.textContent="4"
+    answerOne.textContent="4";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent="4";
+    answerThree.textContent="4";
+    answerFour.textContent=" 4";
+}
+btnQuestionFive.onclick = function (){
+    question.textContent="5"
+    answerOne.textContent="5";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 5";
+    answerThree.textContent=" 5";
+    answerFour.textContent=" 5";
+}
+
+btnQuestionSix.onclick = function (){
+    question.textContent="6"
+    answerOne.textContent="6";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 6";
+    answerThree.textContent=" 6";
+    answerFour.textContent=" 6";
+    
+}
+btnQuestionSeven.onclick = function (){
+    question.textContent="7"
+    answerOne.textContent="7";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 7";
+    answerThree.textContent=" 7";
+    answerFour.textContent=" 7";
+    
+}
+
+btnQuestionEight.onclick = function (){
+    question.textContent="8"
+    answerOne.textContent="8";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 8";
+    answerThree.textContent=" 8";
+    answerFour.textContent=" 8";      
+}
+btnQuestionNine.onclick = function (){
+    question.textContent="9"
+    answerOne.textContent="9";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 9";
+    answerThree.textContent=" 9";
+    answerFour.textContent=" 9";  
+}
+
+btnQuestionTen.onclick = function (){
+    question.textContent="10"
+    answerOne.textContent="10";
+    answerOne.onclick = function(){
+    answerOne.textContent=" errado ";
+    }
+    answerTwo.textContent=" 10";
+    answerThree.textContent=" 10";
+    answerFour.textContent=" 10";  
+}
 
 element.onclick = function (){
-
-    alert("onclick , here ");
-
     text.textContent="Correct question !!";
+    for(i=0;i<qtdQuestion.length;i++){     
+    question.textContent="-"    
+    answerOne.textContent="-";
+    answerTwo.textContent="-";
+    answerThree.textContent="-";
+    answerFour.textContent="-";     
+    }
+}
 
-    for(i=0;i<qtdQuestion.length;i++){
+questionOne.onclick = function(){
+   alert("Certa resposta ");
+}
+// denido a variavél , condicional , incremento 
+for(; qtdQuestion.length<10;){
+     qtdQuestion.length++;
+     qtdQuestion.push(qtdQuestion.length,3);
+     test++;
+}
 
-        console.log("aqui vai fazer a leitura dos vetores");
-        answerOne.textContent=" the segund answer";
+btnOk.onclick = function(){
+
+    alert("OK");
+}
+
+
+
+
+
+/*
+element.onclick = function (){
+    text.textContent="Correct question !!";
+    for(i=0;i<qtdQuestion.length;i++){      
+        if(i==0){              
+        }
         answerTwo.textContent="the segund answer";
         answerThree.textContent="the three answer";
-        answerFour.textContent="the four answer";
-   
+        answerFour.textContent="the four answer";       
+    }
+       if(i == 1){
+        alert("1");
+        answerOne.textContent=" one answer";
+        answerTwo.textContent=" one answer";
+        answerThree.textContent=" one answer";
+        answerFour.textContent=" one answer";
+       }
+
+       if(i==2){
+        alert("2");
+        answerOne.textContent=" two answer";
+        answerTwo.textContent=" two answer";
+        answerThree.textContent=" two answer";
+        answerFour.textContent=" two answer";
+       }
+
+       if(i==3){
+        alert("3");
+        answerOne.textContent=" three answer";
+        answerTwo.textContent=" three answer";
+        answerThree.textContent=" three answer";
+        answerFour.textContent=" three answer";
+       }
+    }
+
+    */
 
     // criar mais um for aqui com as perguntas colocando o incremento fora do for e fazendo outro for em seguida para proxima pergunta
     
@@ -42,33 +214,9 @@ element.onclick = function (){
     // if(&&qtdQuestion == 2){
 
        // answerTwo.textContent="A segunda pergunta"
-    }
-    
 
-    }
+    /*
+           qtdQuestion = qtdQuestion+1;
+ 
 
-    
-
-
-
-    questionOne.onclick = function(){
-
-        alert("Certa resposta ");
-    }
-
-
-
-// denido a variavél , condicional , incremento 
-for(; qtdQuestion.length<10;){
-     qtdQuestion.length++;
-     qtdQuestion.push(qtdQuestion.length,3);
-     test++;
-
-}
-console.log(qtdQuestion);
-console.log(test);
-
-// estrutura para criaçâo do vetor definido varias perguntas para o quizz 
-// new variable vet of qtdQuestion
-// new variable vet of answer 
-// insert the vets em cada indice 
+    */
